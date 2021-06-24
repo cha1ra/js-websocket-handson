12cc0142ff50864234c54e2a6e23c5da087d929e
cha1ra <cha1ra.garnet@gmail.com>
2021-06-25 01:28:30 +0900

    最終形

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http')
-const fs = require('fs') // 追加: ファイルを読み取るモジュール
+const fs = require('fs')
+const WebSocketServer = require('websocket').server
 
 // ポート番号
 const PORT = 3000
 
 // リクエスト・レスポンスの対応内容を記述
 const server = http.createServer((request, response) => {
-  // !!---ここから書き換え---!!
   const url = request.url
   switch (url) {
     case '/':
@@ -20,10 +20,49 @@ const server = http.createServer((request, response) => {
       response.writeHead(404)
       response.end()
   }
-  // !!---ここまで---!!
 })
 
 // リスナーを起動
 server.listen(PORT, () => {
   console.log(`${new Date()} サーバ起動 http://localhost:${PORT}`)
+})
+
+// WebSocketサーバの設定
+const wsServer = new WebSocketServer({
+  httpServer: server,
+  // autoAcceptConnections は本番環境で使っちゃだめ
+  autoAcceptConnections: false
+})
+
+const originIsAllowed = (origin) => {
+  // アクセス元が信頼できるかを検証する用の関数。今回はlocalなので常にtrue
+  return true
+}
+
+wsServer.on('request', (request) => {
+  if (!originIsAllowed(request.origin)) {
+    request.reject()
+    console.log(`${new Date()} ${request.origin} からのアクセスが拒否されました`)
+  }
+
+  const connection = request.accept('ws-sample', request.origin)
+  console.log(`${new Date()} 接続が許可されました`)
+
+  connection.on('message', message => {
+    switch (message.type) {
+      case 'utf8':
+        console.log(`メッセージ: ${message.utf8Data}`)
+        // connection.sendUTF(message.utf8Data) コメントアウト
+        wsServer.broadcast(message.utf8Data) // 追記
+        break
+      case 'binary':
+        console.log(`バイナリデータ: ${message.binaryData.length}byte`)
+        connection.sendBytes(message.binaryData)
+        break
+    }
+  })
+
+  connection.on('close', (reasonCode, description) => {
+    console.log(`${new Date()} ${connection.remoteAddress} が切断されました`)
+  })
 })
