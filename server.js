752077a710c14eb97552d4e455bab5a0f7045e7f
cha1ra <cha1ra.garnet@gmail.com>
2021-06-24 20:49:43 +0900

    HTMLを読み込み、表示

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const http = require('http')
+const fs = require('fs') // 追加: ファイルを読み取るモジュール
 
 // ポート番号
 const PORT = 3000
 
 // リクエスト・レスポンスの対応内容を記述
 const server = http.createServer((request, response) => {
-  response.writeHead(200)
-  response.write('Hello!')
-  response.end()
+  // !!---ここから書き換え---!!
+  const url = request.url
+  switch (url) {
+    case '/':
+      fs.readFile('./public/index.html', 'utf-8', (error, data) => {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.write(data)
+        response.end()
+      })
+      break
+    default:
+      response.writeHead(404)
+      response.end()
+  }
+  // !!---ここまで---!!
 })
 
 // リスナーを起動
